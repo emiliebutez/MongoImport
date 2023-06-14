d70d5a0b5cf5ce0ce7e9ab4b6765906f92f23b03
emiliebutez <63152955+emiliebutez@users.noreply.github.com>
2023-06-14 13:59:45 +0200

    Adding sentiment value mapping and messages.

--- src/main/resources/sentiment-analyzer.py
+++ src/main/resources/sentiment-analyzer.py
@@ -10,13 +10,27 @@ collection = db.tweetsFilteredV4
 sentiments = pipeline("sentiment-analysis", model="nlptown/bert-base-multilingual-uncased-sentiment")
 
 documents = collection.find()
+index = 0
+total_documents = collection.count_documents({})
 
 for document in documents:
     text = document["text"]
     result = sentiments(text)
-    label = result[0]["label"]
+    label_set = set(result[0]["label"])
+    label = ""
+
+    if label_set.intersection({"1", "2"}):
+        label = "Négatif"
+    elif label_set.intersection({"4", "5"}):
+        label = "Positif"
+    else:
+        label = "Neutre"
 
     collection.update_one(
         {"id": document["id"]},
         {"$set": {"sentiment": label}}
     )
+    index += 1
+    print(f"Document treated: {index}/{total_documents}")
+
+print("All documents have been processed.")
