fc7b1a9927b0d08126efae6d1266cf23664399f2
emiliebutez <63152955+emiliebutez@users.noreply.github.com>
2023-06-14 16:35:26 +0200

    Calling pipelines requests.

--- src/main/kotlin/mongojavaimport/Main.kt
+++ src/main/kotlin/mongojavaimport/Main.kt
@@ -31,4 +31,25 @@ fun main() {
 
     val exitCode = process.waitFor()
     println("Python sentiment anlayzer script execution completed. Exit code: $exitCode")
+
+    val directory = File("src/main/resources/requetes_pipelines")
+
+    if (directory.isDirectory) {
+        val pythonScripts = directory.listFiles { file -> file.isFile && file.extension == "py" }
+
+        pythonScripts?.forEach { script ->
+            val process = ProcessBuilder("python", script.absolutePath)
+                .redirectErrorStream(true)
+                .start()
+
+            val reader = BufferedReader(InputStreamReader(process.inputStream))
+            var line: String?
+            while (reader.readLine().also { line = it } != null) {
+                println(line)
+            }
+
+            val exitCode = process.waitFor()
+            println("Python script execution completed for: ${script.name}. Exit code: $exitCode")
+        }
+    }
 }
