e3e27b1673f69573b2bd9cc116059635bfd9dc8e
emiliebutez <63152955+emiliebutez@users.noreply.github.com>
2023-06-15 15:11:45 +0200

    Adding Javadoc in mongo loader class.

--- src/main/kotlin/mongojavaimport/MongoLoader.kt
+++ src/main/kotlin/mongojavaimport/MongoLoader.kt
@@ -8,6 +8,9 @@ import com.mongodb.client.MongoClients;
 import org.bson.Document
 import com.mongodb.client.model.InsertOneModel;
 
+/**
+ * The `MongoLoader` class is responsible for loading JSON data into a MongoDB database.
+ */
 class MongoLoader {
     private val objectMapper = ObjectMapper()
         .configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
@@ -16,10 +19,22 @@ class MongoLoader {
     val database = client.getDatabase("tweetsDL");
     val coll = database.getCollection("tweetsDL");
 
+    /**
+     * Filters the relevant properties from the JSON and deserializes it into a `Tweet` object.
+     *
+     * @param json The JSON string to be filtered and deserialized
+     * @return The deserialized `Tweet` object
+     */
     fun filterProperties(json: String): Tweet {
         return objectMapper.readValue(json, Tweet::class.java)
     }
 
+    /**
+     * Loads the JSON data from a file into the MongoDB database.
+     * Only tweets containing relevant hashtags are inserted into the database.
+     *
+     * @param file The file containing the JSON data
+     */
     fun loadJsonIntoDb(file: File) {
         val content = file.readLines()
             .parallelStream()
