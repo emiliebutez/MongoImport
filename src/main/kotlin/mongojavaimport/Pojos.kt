df72e088015c972daf07913a518cfe2c7d5b71aa
emiliebutez <63152955+emiliebutez@users.noreply.github.com>
2023-06-15 14:59:51 +0200

    Adding Javadoc.

--- src/main/kotlin/mongojavaimport/Pojos.kt
+++ src/main/kotlin/mongojavaimport/Pojos.kt
@@ -5,6 +5,9 @@ import java.io.File
 import java.text.SimpleDateFormat
 import java.util.*
 
+/**
+ * Represents a tweet object.
+ */
 data class Tweet(
     val _id: ObjectId = ObjectId.get(),
     val created_at: String,
@@ -12,10 +15,10 @@ data class Tweet(
     val id_str: String,
     val text: String,
     val source: String,
-    val in_reply_to_status_id: Any?, // null in the provided data
-    val in_reply_to_status_id_str: Any?, // null in the provided data
-    val in_reply_to_user_id: Any?, // null in the provided data
-    val in_reply_to_user_id_str: Any?, // null in the provided data
+    val in_reply_to_status_id: Any?,
+    val in_reply_to_status_id_str: Any?,
+    val in_reply_to_user_id: Any?,
+    val in_reply_to_user_id_str: Any?,
     val user: FilteredUser,
     val retweeted_status: FilteredRetweetedStatus? = null,
     val is_quote_status: Boolean,
@@ -28,28 +31,12 @@ data class Tweet(
     val hour: String? = getHour(created_at),
     val date: String? = getDate(created_at),
     val sentiment: String? = null,
-    val equipes: List<String>? = getEquipe(text),
+    val equipes: List<String>? = getEquipes(text),
 )
 
-fun getHour(date: String) : String {
-    val dateFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH)
-    val date = dateFormat.parse(date)
-    val hourFormat = SimpleDateFormat("HH:mm", Locale.ENGLISH)
-    return hourFormat.format(date)
-}
-
-fun getDate(date: String) : String {
-    val dateFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH)
-    val date = dateFormat.parse(date)
-    val dayFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
-    return dayFormat.format(date)
-}
-
-fun getEquipe(textTweet: String): List<String> {
-    val equipes = Equipes.values().map { it.name }
-    return equipes.filter { textTweet.contains(it, ignoreCase = true) }
-}
-
+/**
+ * Represents a filtered user object.
+ */
 data class FilteredUser(
     val id: Long,
     val id_str: String,
@@ -64,10 +51,13 @@ data class FilteredUser(
     val favourites_count: Int,
     val statuses_count: Int,
     val created_at: String,
-    val lang: Any?, // null in the provided data
+    val lang: Any?,
     val date: String? = getDate(created_at),
 )
 
+/**
+ * Represents a filtered retweeted status object.
+ */
 data class FilteredRetweetedStatus(
     val created_at: String,
     val id: Long,
@@ -75,10 +65,10 @@ data class FilteredRetweetedStatus(
     val text: String,
     val display_text_range: List<Int>? = null,
     val source: String,
-    val in_reply_to_status_id: Any?, // null in the provided data
-    val in_reply_to_status_id_str: Any?, // null in the provided data
-    val in_reply_to_user_id: Any?, // null in the provided data
-    val in_reply_to_user_id_str: Any?, // null in the provided data
+    val in_reply_to_status_id: Any?,
+    val in_reply_to_status_id_str: Any?,
+    val in_reply_to_user_id: Any?,
+    val in_reply_to_user_id_str: Any?,
     val user: FilteredUser,
     val is_quote_status: Boolean,
     val quote_count: Int,
@@ -95,20 +85,28 @@ data class FilteredRetweetedStatus(
     val date: String? = getDate(created_at),
 )
 
+/**
+ * Represents a entities object.
+ */
 data class Entities(
-    val hashtags: List<Hashtag>, // Empty list in the provided data
-    val urls: List<Any>, // Empty list in the provided data
+    val hashtags: List<Hashtag>,
+    val urls: List<Any>,
     val user_mentions: List<UserMention>,
-    val symbols: List<Any>, // Empty list in the provided data
+    val symbols: List<Any>,
     val media: List<Media> = emptyList(),
 )
 
-//
+/**
+ * Represents a hashtag object.
+ */
 data class Hashtag(
     val indices: List<Int>,
     val text: String
 )
 
+/**
+ * Represents a user mention object.
+ */
 data class UserMention(
     val screen_name: String? = null,
     val name: String? = null,
@@ -117,6 +115,9 @@ data class UserMention(
     val indices: List<Int>? = null,
 )
 
+/**
+ * Represents a media object.
+ */
 data class Media(
     val id: Long,
     val id_str: String,
@@ -128,3 +129,40 @@ data class Media(
     val expanded_url: String,
     val type: String,
 )
+
+/**
+ * Retrieves the hour from a given date string.
+ *
+ * @param date The date string to extract the hour from
+ * @return The hour in HH:mm format
+ */
+fun getHour(date: String) : String {
+    val dateFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH)
+    val date = dateFormat.parse(date)
+    val hourFormat = SimpleDateFormat("HH:mm", Locale.ENGLISH)
+    return hourFormat.format(date)
+}
+
+/**
+ * Retrieves the date from a given date string.
+ *
+ * @param date The date string to extract the date from
+ * @return The date in dd/MM/yyyy format
+ */
+fun getDate(date: String) : String {
+    val dateFormat = SimpleDateFormat("EEE MMM dd HH:mm:ss Z yyyy", Locale.ENGLISH)
+    val date = dateFormat.parse(date)
+    val dayFormat = SimpleDateFormat("dd/MM/yyyy", Locale.ENGLISH)
+    return dayFormat.format(date)
+}
+
+/**
+ * Retrieves the list of teams mentioned in a tweet.
+ *
+ * @param textTweet The content of the tweet
+ * @return The list of teams mentioned in the tweet
+ */
+fun getEquipes(textTweet: String): List<String> {
+    val equipes = Equipes.values().map { it.name }
+    return equipes.filter { textTweet.contains(it, ignoreCase = true) }
+}
